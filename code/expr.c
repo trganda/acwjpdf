dbb1bd9c1f55a964abea3638f439ef50ef2d730e
trganda <jongsx1203@gmail.com>
2021-06-06 16:30:03 +0800

    code update

--- code/expr.c
+++ code/expr.c
@@ -40,29 +40,53 @@ int arithop(int tok) {
   }
 }
 
-// Return an AST tree whose root is a binary operator
-struct ASTnode *binexpr(void) {
+// Operator precedence for each token
+static int OpPrec[] = { 0, 10, 10, 20, 20, 0 };
+
+// Check that we have a binary operator and
+// return its precedence.
+static int op_precedence(int tokentype) {
+  int prec = OpPrec[tokentype];
+  if (prec == 0) {
+    fprintf(stderr, "syntax error on line %d, token %d\n", Line, tokentype);
+    exit(1);
+  }
+  return (prec);
+}
+
+// Return an AST tree whose root is a binary operator.
+// Parameter ptp is the previous token's precedence.
+struct ASTnode *binexpr(int ptp) {
   struct ASTnode *n, *left, *right;
-  int nodetype;
+  int tokentype;
 
   // Get the integer literal on the left.
   // Fetch the next token at the same time.
   left = primary();
 
   // If no tokens left, return just the left node
-  if (Token.token == T_EOF)
+  tokentype = Token.token;
+  if (tokentype == T_EOF)
     return (left);
 
-  // Convert hte token into a node type
-  nodetype = arithop(Token.token);
+  while (op_precedence(tokentype) > ptp) {
+    // Fetch in the next integer literal
+    scan(&Token);
 
-  // Get the next token in
-  scan(&Token);
+    // Recursively call binexpr() with the
+    // precedence of our token to build a sub-tree
+    right = binexpr(op_precedence(tokentype));
 
-  // Recursively get the right-hand tree
-  right = binexpr();
+    // Join that sub-tree with ours. Convert the token
+    // into an AST operation at the same time.    
+    left = mkastnode(arithop(tokentype), left, right, 0);
 
-  // Now build a tree with both sub-trees
-  n = mkastnode(nodetype, left, right, 0);
-  return (n);
+    // Update the details of the current token.
+    // If no tokens left, return just the left node
+    tokentype = Token.token;
+    if (tokentype == T_EOF)
+      return (left);
+  }
+
+  return (left);
 }
